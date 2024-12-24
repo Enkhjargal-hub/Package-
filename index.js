6d454465873510e15ad8bf984422735574168bc3
Enkhjargal-hub <Joloenja@gmail.com>
2024-12-24 10:57:38 +0800

    h

---
+++ index.js
@@ -0,0 +1,6 @@
+function printMsg() {
+    console.log("Энэ бол миний модулийн мессеж");
+  }
+  
+  module.exports = { printMsg };
+  
---
+++ test.js
@@ -0,0 +1,2 @@
+const { printMsg } = require('my-module');
+printMsg(); // Энэ бол миний модулийн мессеж
